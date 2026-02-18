f3f67eeda6efb0ee1cafa6fb0b4b9645ae646adc
ohjhas <spliteritoob@gmail.com>
2026-02-18 18:29:39 -0300

    design overhaul

--- Build.sh
+++ Build.sh
@@ -1,3 +1,8 @@
 echo Build using mkarchiso
+echo OmegaLinux NEXT build ¡START!
 rm -rf ISOs/*
 sudo mkarchiso -v -r -w WorkDir -o ISOs Sources/
+echo OmegaLinux NEXT build ¡FINISH!
+((sec=SECONDS%60, min=SECONDS/60%60, hrs=SECONDS/3600))
+timestamp=$(printf "OLN Build took %02d hours, %02d minutes, and %02d seconds." $hrs $min $sec)
+echo $timestamp
