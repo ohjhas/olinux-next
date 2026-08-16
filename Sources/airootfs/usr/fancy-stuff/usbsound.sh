81b0f5e8b1e9d2029f70084f18b4e30191250e4a
unsoil <spliteritoob@gmail.com>
2026-08-16 20:27:41 -0300

    play sound when connecting or disconnecting usb devices

---
+++ Sources/airootfs/usr/fancy-stuff/usbsound.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CONNECT_SOUND="/usr/share/sounds/freedesktop/stereo/device-added.oga"
+DISCONNECT_SOUND="/usr/share/sounds/freedesktop/stereo/device-removed.oga"
+
+play_sound() {
+    play "$1"
+}
+
+action=""
+devtype=""
+
+udevadm monitor --udev --subsystem-match=usb --property | while read -r line; do
+    if [ -z "$line" ]; then
+        if [ "$devtype" = "usb_device" ]; then
+            if [ "$action" = "add" ]; then
+                play_sound "$CONNECT_SOUND"
+            elif [ "$action" = "remove" ]; then
+                play_sound "$DISCONNECT_SOUND"
+            fi
+        fi
+        action=""
+        devtype=""
+        continue
+    fi
+
+    case "$line" in
+        ACTION=*) action="${line#ACTION=}" ;;
+        DEVTYPE=*) devtype="${line#DEVTYPE=}" ;;
+    esac
+done
--- Sources/profiledef.sh
+++ Sources/profiledef.sh
@@ -12,7 +12,8 @@ bootmodes=('bios.syslinux'
            'uefi.grub')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'lz4')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -27,4 +28,5 @@ file_permissions=(
   ["/usr/fancy-stuff/bootsd.sh"]="0:0:755"
   ["/usr/fancy-stuff/freemyram.sh"]="0:0:755"
   ["/usr/fancy-stuff/lxpanel.sh"]="0:0:755"
+  ["/usr/fancy-stuff/usbsound.sh"]="0:0:755"
 )
