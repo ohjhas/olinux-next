c065073b03c60f5c69b1e07e62538796b16e98df
unsoil <spliteritoob@gmail.com>
2026-08-16 00:45:39 -0300

    logout banner

--- Sources/airootfs/usr/fancy-stuff/fixups.sh
+++ Sources/airootfs/usr/fancy-stuff/fixups.sh
@@ -17,5 +17,5 @@ rm -rf /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
 cp -rf /usr/fancy-stuff/dialog-warning.ogg /usr/share/sounds/freedesktop/stereo/
 
 # Now we do this shit: we change the logout banner on the installed environment but that means the ugly ass-looking lxde banner on the live env is still there, fck
-rm -rf /usr/share/lxde/images/logout-banner.png
-cp -rf /usr/fancy-stuff/logout-banner.png /usr/share/lxde/images/
+# rm -rf /usr/share/lxde/images/logout-banner.png
+# cp -rf /usr/fancy-stuff/logout-banner.png /usr/share/lxde/images/
