25762bcea6b7ae5f2316b106c988879c5f51bcb2
unsoil <spliteritoob@gmail.com>
2026-08-16 04:22:00 -0300

    remove xscreensaver patch since already patched

--- Sources/airootfs/usr/fancy-stuff/fixups.sh
+++ Sources/airootfs/usr/fancy-stuff/fixups.sh
@@ -2,10 +2,6 @@
 # OmegaLinux NEXT Fixup script that runs before finishing calamares installation, hopefully i can do better after release.
 # 15 February 2026
 
-# Fix xscreensaver lock bug where the user cant unlock, with an authentication fail error.
-rm -rf /etc/pam.d/xscreensaver
-cp -rf /usr/fancy-stuff/xsc/xscreensaver /etc/pam.d/
-
 # Populate arch keyrings, without this the average user wouldnt be able to even use OLN at all.
 pacman-key --init
 pacman-key --populate archlinux
@@ -15,7 +11,3 @@ pacman -Sy
 # And honestly i wanted to 
 rm -rf /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
 cp -rf /usr/fancy-stuff/dialog-warning.ogg /usr/share/sounds/freedesktop/stereo/
-
-# Now we do this shit: we change the logout banner on the installed environment but that means the ugly ass-looking lxde banner on the live env is still there, fck
-# rm -rf /usr/share/lxde/images/logout-banner.png
-# cp -rf /usr/fancy-stuff/logout-banner.png /usr/share/lxde/images/
